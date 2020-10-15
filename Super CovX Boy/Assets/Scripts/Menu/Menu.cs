0c7d5fb21c4bbde9e72c07dd26208593c176a899
Arroshi <59584423+Arroshi@users.noreply.github.com>
2020-10-15 05:43:22 -0500

    Menu + Sonid+ SaveVolumen

---
+++ Super CovX Boy/Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject Mmenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.anyKey == true)
+        {
+            Mmenu.SetActive(true);
+            gameObject.SetActive(false);
+        }*/
+        //EffectsSounds.instance.PlaySong(EffectsSounds.instance.menuMusic);
+    }
+
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);        
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Salir");
+    }
+
+
+}
---
+++ Super CovX Boy/Assets/Scripts/Sonidos/EffectsSounds.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EffectsSounds : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioSource effectSource;    
+
+    //  public AudioClip introMusic;
+    public AudioClip menuMusic;
+    public AudioClip effectClick;
+
+    public Slider Volumen, Effects;
+
+    public static EffectsSounds instance;
+
+    
+    /*void Awake()
+    {
+        instance = this;
+        InitializedVolumen();
+    }*/
+
+
+    void Start()
+    {
+        
+        InitializedVolumen();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void InitializedVolumen()
+    {
+        musicSource.volume = PlayerPrefs.GetFloat("musicVolume", 1.0f);
+        effectSource.volume = PlayerPrefs.GetFloat("effectVolume", 1.0f);
+
+        Volumen.value = musicSource.volume;
+        Effects.value = effectSource.volume;
+    }
+
+    public void PlayEffects()
+    {
+        effectSource.PlayOneShot(effectClick);
+    }
+
+    public void PlaySong(AudioClip audioClip)
+    {
+        musicSource.clip = audioClip;
+        musicSource.Play();
+    }
+
+    public void VolumenSlider()
+    {
+        musicSource.volume = Volumen.value;
+        PlayerPrefs.SetFloat("musicVolume", musicSource.volume);
+        PlayerPrefs.Save();
+    }
+
+    public void EffectSlider()
+    {
+        effectSource.volume = Effects.value;
+        PlayerPrefs.SetFloat("effectVolume", effectSource.volume);
+        PlayerPrefs.Save();
+    }
+}
