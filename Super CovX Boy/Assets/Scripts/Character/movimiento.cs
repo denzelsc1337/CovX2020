243bdb3c9a0dc71deceefd55f91b8628bf1cb33e
Arroshi <59584423+Arroshi@users.noreply.github.com>
2020-10-19 05:57:17 -0500

    VirtualGuy

--- Super CovX Boy/Assets/Scripts/Character/movimiento.cs
+++ Super CovX Boy/Assets/Scripts/Character/movimiento.cs
@@ -8,17 +8,24 @@ public class movimiento : MonoBehaviour
     public float runspeed;
     public float jumpspeed;
     private Rigidbody2D rb2D;
+
+    public SpriteRenderer SpriteRenderer;
+
+
     public static float conteo = 0.0f;
     public static float total = 0;
-    private Button Izquierdass, Derechass;
-    private Animator m_animacion;
+    
+    public Animator animator;
     public static float jumps;
+
+    public bool betterJump = false;
+    public float fallMultiplier = 0.5f;
+    public float lowJumpMultiplier = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
-        m_animacion = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -39,14 +46,18 @@ public class movimiento : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D) || Input.GetKey("right"))
         {
-            rb2D.AddForce(new Vector2(runspeed, rb2D.velocity.y));
-            //rb2D.velocity = new Vector2(runspeed, rb2D.velocity.y);
+            //rb2D.AddForce(new Vector2(runspeed, rb2D.velocity.y));
+            rb2D.velocity = new Vector2(runspeed, rb2D.velocity.y);
+            SpriteRenderer.flipX = false;
+            animator.SetBool("Run", true);
             //runspeed++;
         }
         else if (Input.GetKey("a") || Input.GetKey("left"))
         {
-            rb2D.AddForce(new Vector2(-runspeed, rb2D.velocity.y));
-            //rb2D.velocity = new Vector2(-runspeed, rb2D.velocity.y);
+            //rb2D.AddForce(new Vector2(-runspeed, rb2D.velocity.y));
+            rb2D.velocity = new Vector2(-runspeed, rb2D.velocity.y);
+            SpriteRenderer.flipX = true;
+            animator.SetBool("Run", true);
             //runspeed++;
 
         }
@@ -54,6 +65,7 @@ public class movimiento : MonoBehaviour
         {
             //rb2D.AddForce(new Vector2(0, rb2D.velocity.y));
             rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+            animator.SetBool("Run", false);
         }
 
         //if (Input.GetKeyUp("d") || runspeed > 200)
@@ -76,6 +88,28 @@ public class movimiento : MonoBehaviour
             rb2D.velocity = new Vector2(rb2D.velocity.x, jumpspeed);
             conteo++;
             total = jumpsCounter.jumpValue = conteo;
+        }
+
+        if (checkGround.isGrounded == false)
+        {
+            animator.SetBool("Jump", true);
+            animator.SetBool("Run", false);
+        }
+        if (checkGround.isGrounded == true)
+        {
+            animator.SetBool("Jump", false);
+        }
+
+        if (betterJump)
+        {
+            if (rb2D.velocity.y < 0)
+            {
+                rb2D.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier) * Time.deltaTime;
+            }
+            if (rb2D.velocity.y > 0 && !Input.GetKey("space"))
+            {
+                rb2D.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier) * Time.deltaTime;
+            }
         }
     }
 
